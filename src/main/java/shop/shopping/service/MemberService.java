3402d5a246f69e241b87f133570430a4d54540d1
yujinshin <ssy66822@naver.com>
2024-03-14 06:44:15 +0900

    첨부터 다시 다 갈아엎으려고 한다

--- src/main/java/shop/shopping/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package shop.shopping.service;
-
-import shop.shopping.dto.MemberSignUpRequestDto;
-
-public interface MemberService {
-
-    // 회원가입
-    public Long signUp(MemberSignUpRequestDto requestDto) throws Exception;
-}
